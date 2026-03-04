38ff465b00b6533bfde1271bac75de3c78926b30
Pinhogus <gustavooliveirapinho@gmail.com>
2026-03-04 13:58:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ event_id = "15479162"
 url = f"https://api.sofascore.com/api/v1/event/{event_id}/graph"
 
 headers = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Accept": "application/json, text/plain, */*",
+    "Referer": "https://www.sofascore.com/",
+    "Origin": "https://www.sofascore.com",
+    "Connection": "keep-alive"
 }
 
 response = requests.get(url, headers=headers)
 
 print(response.status_code)
-print(response.json())
+print(response.text)
